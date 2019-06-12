31dc3083ca186319620c9faf978ea6f07167944d
xys1223553317 <1223553317@qq.com>
2019-06-12 14:27:31 +0800

    框架

---
+++ login_ MyCenter/src/main/java/com/jk/xys/XysApplication.java
@@ -0,0 +1,13 @@
+package com.jk.xys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XysApplication.class, args);
+    }
+
+}
